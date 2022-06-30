f3906445cc6f9f2d11ae1632cb1947c80d8bc692
Sam <58091896+computesam@users.noreply.github.com>
2022-06-30 17:28:02 -0400

    Add files via upload

---
+++ Views/Display.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Tesla_Demo
+{
+    /// <summary>
+    /// Interaction logic for Display.xaml
+    /// </summary>
+    public partial class Display : Page
+    {
+        public Display()
+        {
+            InitializeComponent();
+        }
+    }
+}
